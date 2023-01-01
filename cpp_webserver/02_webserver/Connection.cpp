6e145e00abbebb29a129acaa03cd87eb9999323f
whaleshade <whaleshade@gmail.com>
2023-01-01 15:00:42 +0900

    NEW : webserver .. client read error remains..

---
+++ cpp_webserver/02_webserver/Connection.cpp
@@ -0,0 +1,107 @@
+/***************************************************/
+/* CODED BY JIN H. BANG ===========================*/
+/***************************************************/
+
+#include "Connection.hpp"
+
+class Response;
+
+Connection::Connection()
+{}
+
+Connection::~Connection()
+{}
+
+void
+Connection::connectionLoop()
+{
+	_eventManager.declareKqueue();
+
+
+	std::cout << "	::map size : " << _serverMap.size() << "\n" << "	::serverSockets: " << _serverSockets.size() << "\n";
+
+	std::vector<int>::iterator it;
+	for (it = _serverSockets.begin(); it != _serverSockets.end(); ++it){
+		_eventManager.enrollEventToChangeList(_serverMap[*it]._serverSocket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+		std::cout << "	enrolled serv socket : " << *it << "\n";
+	}
+
+	int eventsNum = 0;
+	struct kevent const *currEvent = nullptr;
+	while (true)
+	{
+		eventsNum = _eventManager.senseEvents();
+		_eventManager.clearChangeList();
+		for (int i = 0; i < eventsNum; ++i)
+		{
+
+			currEvent = const_cast<struct kevent const *>(&(_eventManager.getEventList()[i]));
+
+			/* error case */
+			if ((currEvent->flags & EV_ERROR || currEvent->flags & EV_EOF)) {
+				if (_serverMap.find(currEvent->ident) != _serverMap.end()) {
+					close(currEvent->ident);
+					std::cerr << " server error case \n";
+					throw ConnectionError();
+				}
+				else if (_clientMap.find(currEvent->ident) != _clientMap.end())
+				{
+					close(currEvent->ident);
+					std::cerr << " client error case \n";
+					throw ConnectionError();
+				}
+			}
+
+			/* read event */
+			if (currEvent->flags & EVFILT_READ)
+			{
+				if (_serverMap.find(currEvent->ident) != _serverMap.end())
+				{
+					int clientSocket = accept(currEvent->ident, (sockaddr *)&_serverMap[currEvent->ident]._serverAddr, &_serverMap[currEvent->ident]._serverAddrLen);
+					std::cout << "	accepted client socket : " << clientSocket << "\n"; // test code
+					if (clientSocket == FAIL){
+						std::cerr << " Error : accept() \n";
+						throw ConnectionError();}
+
+					if (fcntl(clientSocket, F_SETFL, O_NONBLOCK) == FAIL){
+						std::cerr << " Error : fcntl() \n";
+						throw ConnectionError();}
+
+					_eventManager.enrollEventToChangeList(clientSocket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+					_eventManager.enrollEventToChangeList(clientSocket, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, NULL);
+
+					_serverMap[currEvent->ident]._clients.push_back(clientSocket);
+					InfoClient infoClient; // need to be initialized
+					infoClient._clientSocket = clientSocket;
+					infoClient._server = &_serverMap[currEvent->ident];
+					_clientMap.insert(std::pair<int, InfoClient>(clientSocket, infoClient));
+				}
+
+				if (_clientMap.find(currEvent->ident) != _clientMap.end()) {
+					char buffer[BUFFER_SIZE] = {0};
+
+					std::cout << "	clientMap size : " << _clientMap.size() << "\n";
+
+					int valRead = read(currEvent->ident, buffer, BUFFER_SIZE);
+					if (valRead == FAIL)// && errno != EAGAIN)
+					{
+						std::cerr << " from client " << currEvent->ident ;
+						std::cerr << " Error : read() \n";
+						// throw ConnectionError();
+					}
+					std::cout << buffer << "\n";
+					_clientMap[currEvent->ident].reqMsg = buffer;
+				}
+			}
+
+			/* write event */
+			if (currEvent->flags & EVFILT_WRITE)
+			{
+				if (_clientMap.find(currEvent->ident) != _clientMap.end()) {
+					Response responser;
+					responser.responseToClient(currEvent->ident, _clientMap[currEvent->ident]);
+				}
+			}
+		}
+	}
+}
