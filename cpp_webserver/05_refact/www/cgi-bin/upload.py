9cf5a0f0573de8f084d0bebb6cb51f1a8d6d926e
whaleshade <whaleshade@gmail.com>
2023-01-27 17:50:22 +0900

    post.py

--- cpp_webserver/05_refact/www/cgi-bin/upload.py
+++ cpp_webserver/05_refact/www/cgi-bin/upload.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from distutils.command.upload import upload
 import sys, os
+import shutil
+
 
 content = sys.stdin.read()
 content = content.split("\r\n")
@@ -33,7 +35,7 @@ print("<h4>")
 
 print("</h4>")
 print("<div>")
-if (content_type.split(': ')[1] == "text/plain"):
+if (content_type.split(': ')[1] != "image/jpeg"):
 	upload_file = open(upload_path + filename, "w")
 	upload_file.write(file_content)
 	upload_file.close()
@@ -43,9 +45,7 @@ if (content_type.split(': ')[1] == "text/plain"):
 elif content_type.split(': ')[1] == "image/jpeg":
 	with open(upload_path + filename, "w") as f:
 		print(file_content)
-		print("\n -> end of cotent")
 		f.write(bytes(file_content, 'utf-8'))
-		# f.write(file_content)
 
 	print("<h1>")
 	print("FILE UPLOADED!!")
@@ -82,64 +82,3 @@ print("</html>")
 
 # form = cgi.FieldStorage()
 # print(form)
-
-# print("POST.PY!!")
-
-# body = sys.stdin.read()
-# print(body)
-
-# method = os.environ['REQUEST_METHOD']
-# print(method)
-
-# form = cgi.FieldStorage()
-# print(form)
-
-# # Parse the form data
-# form = cgi.FieldStorage()
-# print(form)
-# # Extract the uploaded files
-# files = {}
-# for field in form.keys():
-#     # Check if the field is a file field
-#     if isinstance(form[field], cgi.FieldStorage) and form[field].filename:
-#         # Save the file to the specified location
-#         with open(save_path + form[field].filename, 'wb') as f:
-#             f.write(form[field].file.read())
-#         files[field] = form[field].filename
-
-# if len(files) > 0:
-#     message = "file uploaded success"
-# else:
-#     message = "file uploaded failed"
-
-# # Print the uploaded files
-# response = "<html><body><center>"
-# response += "<h1>{}<h1>".format(message)
-# for field, filename in files.items():
-#     response += "<p>{}: {}</p>\n".format(field, filename)
-# response += "</center></body></html>"
-
-# print("\r")
-# print(response)
-
-# exit()
-
-
-# #!/usr/bin/python3
-
-# # Import modules for CGI handling
-# import cgi, io, sys
-
-# # res = sys.stdin.read()
-# # print(res)
-
-# content = sys.stdin.read()
-# content_list = content.split("\r\n")
-# # print(content_list[0][2:])
-
-# ret = cgi.parse_multipart(io.BytesIO(bytes(content)), {"boundary": bytes(content_list[0][2:])})
-# print(ret)
-
-# # form = cgi.FieldStorage()
-
-# # print(form)
