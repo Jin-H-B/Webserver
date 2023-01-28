f7838ec61bd72184cf3cee39a349b7277d493e66
whaleshade <whaleshade@gmail.com>
2023-01-28 14:23:08 +0900

    delete +x

--- cpp_webserver/05_refact/www/cgi-bin/delete.py
+++ cpp_webserver/05_refact/www/cgi-bin/delete.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-# import cgi, cgitb , sys
+import cgi, cgitb , sys
 
 # # Create instance of FieldStorage
-# form = cgi.FieldStorage()
+form = cgi.FieldStorage()
 print("<html>")
 print("<body>")
-# print(form)
+print(form)
 print("hello")
 print("<h1>")
 print("delete")
